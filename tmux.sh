c427c43b0ea8ea5e9e0ae610e773445ef22a651d
Sam <sdvongsay@gmail.com>
2018-08-02 00:27:22 -0700

    Modified script to open ide-like tmux

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-tmux new -s Session
-tmux split-window -h
-tmux select-pane -t 0
-tmux split-window -v
-tmux select-pane -t 0
-tmux resize-pane -D 20
+session="ide"
 
-tmux attach -t Session
+# Start up tmux
+tmux start-server
+
+# Create new session and start vim from saved session in new window
+tmux new-session -d -s $session -n vim
+
+# Select pane 1
+tmux selectp -t 1
+tmux send-keys "cd ~/Workspace/;vim" C-m
+
+# Split pane 1 horizontally by 70%
+tmux splitw -v -p 30
+
+# Select pane 2
+tmux selectp -t 2
+tmux send-keys "cd ~/Workspace" C-m
+
+# Select pane 1
+tmux selectp -t 1
+
+# Return to main window
+tmux select-window -t $session:0
+
+# Finished setup; attach to tmux session
+tmux attach-session -t $session
